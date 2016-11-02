83ea056c8cdd818cba3c49265b1281430ff72245
deadlyfingers <david@deadlyfingers.com>
2016-11-02 14:50:42 +0000

    UI scripts and prefabs for integration

---
+++ HoloAssistedTraining/Assets/Code/Managers/ScoreManager.cs
@@ -0,0 +1,103 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using HoloToolkit.Unity;
+
+public class ScoreManager : Singleton<ScoreManager>
+{
+
+    [SerializeField]
+    Scoring scoring;
+
+    public Text scoreText;
+
+    private uint _score;
+    public uint score
+    {
+        get
+        {
+            return _score;
+        }
+        set
+        {
+            _score = value;
+            UpdateScoreText();
+        }
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+        Reset();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void UpdateScoreText()
+    {
+        scoreText.text = score.ToString();
+    }
+
+    void Reset()
+    {
+        score = 0;
+    }
+
+    void OnReset()
+    {
+        Debug.Log("Score: Reseting player");
+        Reset();
+    }
+
+    void OnCalculateFinalScore(float timeRemaining)
+    {
+        // only update score with bonus if time remaining
+        if (timeRemaining > 0)
+        {
+            score = (uint)Mathf.Round(score * timeRemaining * scoring.timeBonusMultiplier);
+            Debug.Log(string.Format("Score bonus time: {0} x {1}", timeRemaining, scoring.timeBonusMultiplier));
+        }
+        Debug.Log(string.Format("Score final: {0}", score));
+    }
+
+    void OnScoreAttempts(uint attempts)
+    {
+        uint attemptScore = GetScore(attempts);
+        score += attemptScore;
+        Debug.Log(string.Format("Attempt {0} Score: {1} Total: {2}", attempts, attemptScore, score));
+    }
+
+    uint GetScore(uint attempts)
+    {
+        uint attemptScore = scoring.more;
+        switch (attempts)
+        {
+            case 1:
+                attemptScore = scoring.first;
+                break;
+            case 2:
+                attemptScore = scoring.second;
+                break;
+            case 3:
+                attemptScore = scoring.third;
+                break;
+        }
+        return attemptScore;
+    }
+}
+
+// Scoring model
+[System.Serializable]
+public class Scoring
+{
+    // scoring for each attempt
+    public uint first = 100; // maximum score
+    public uint second = 50;
+    public uint third = 25;
+    public uint more = 10; // minimun score
+
+    public float timeBonusMultiplier = 1.0f;
+}
---
+++ HoloAssistedTraining/Assets/Code/Managers/UIManager.cs
@@ -0,0 +1,172 @@
+﻿using UnityEngine;
+using System.Collections;
+using HoloToolkit.Unity;
+using UnityEngine.UI;
+
+public class UIManager : Singleton<UIManager>
+{
+    [Header("Progress Bar")]
+    public Image progressBar;
+    public uint totalSteps = 10;
+    private uint _currentStep;
+    public uint currentStep
+    {
+        get
+        {
+            return _currentStep;
+        }
+        set
+        {
+            _currentStep = value;
+            UpdateProgressBar();
+        }
+    }
+
+    [Header("Clock Timer")]
+    public Image clockImage;
+    public Image clockImageBackground;
+    public uint totalSeconds; // the time to complete the task where bonus points will be awarded
+    private float _elapsedTime;
+    public float elapsedTime
+    {
+        get
+        {
+            return _elapsedTime;
+        }
+        set
+        {
+            _elapsedTime = value;
+            UpdateClockFillSegment();
+        }
+    }
+    private bool stopTheClock; // stop counting time elapsed when finished
+
+    // Use this for initialization
+    void Start()
+    {
+        // Set the scene
+        Reset();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Recording total time taken to complete. If this is not needed then add the conditional (elapsedTime < totalSeconds) to stop checking.
+        if (!stopTheClock)
+        {
+            elapsedTime += Time.deltaTime;
+        }
+    }
+
+    private void UpdateProgressBar()
+    {
+        float zeroToOne = (float)currentStep / (float)totalSteps;
+        RectTransform bar = progressBar.GetComponent<RectTransform>();
+        if (zeroToOne < 0)
+        {
+            zeroToOne = 0;
+            Debug.LogWarning("Progress less than 0.0");
+        }
+        else if (zeroToOne > 1)
+        {
+            zeroToOne = 1;
+            Debug.LogWarning("Progress greater than 1.0");
+        }
+        Debug.Log(string.Format("Progress {0} value: {1}", currentStep, zeroToOne));
+        bar.localScale = new Vector3(zeroToOne, 1, 1);
+    }
+
+    private void UpdateClockFillSegment()
+    {
+        // countdown from 1.0 to 0.0
+        float zeroToOne = 1 - elapsedTime / (float)totalSeconds;
+        if (zeroToOne < 0)
+        {
+            zeroToOne = 0;
+            return; //Debug.LogWarning("Clock fill segment less than 0.0");
+        }
+        else if (zeroToOne > 1)
+        {
+            zeroToOne = 1;
+            return; //Debug.LogWarning("Clock fill segment greater than 1.0");
+        }
+        //Debug.Log(string.Format("Time {0}/{1} value: {2}", elapsedTime, totalSeconds, zeroToOne));
+        clockImage.fillAmount = zeroToOne;
+        // change background color depending on time remaining
+        if (zeroToOne < 0.2)
+        {
+            clockImageBackground.GetComponent<Image>().color = Color.red;
+        }
+        else if (zeroToOne < 0.5)
+        {
+            clockImageBackground.GetComponent<Image>().color = Color.yellow;
+        }
+        else
+        {
+            clockImageBackground.GetComponent<Image>().color = Color.green;
+        }
+    }
+
+    private void Reset(bool isRepeat = false)
+    {
+        currentStep = 0;
+        elapsedTime = 0.0f;
+        stopTheClock = false;
+
+    }
+
+    private bool Next()
+    {
+        if (currentStep > totalSteps)
+        {
+            return false;
+        }
+
+        currentStep = currentStep + 1;
+        if (currentStep < totalSteps)
+        {
+            return true;
+        }
+        else if (currentStep == totalSteps)
+        {
+            stopTheClock = true;
+        }
+        return (currentStep <= totalSteps) ? true : false; // whether step should score
+    }
+
+    /// <summary>
+    /// Button Handlers
+    /// </summary>
+
+    public void OnResetTapped()
+    {
+        Reset(true);
+        SendMessageUpwards("OnReset");
+    }
+
+    public void OnReset()
+    {
+        Reset(true);
+    }
+
+    public void OnNext(uint attempts)
+    {
+        if (Next())
+        {
+            SendMessageUpwards("OnScoreAttempts", attempts);
+        }
+        // calculate final score when finished
+        if (currentStep == totalSteps)
+        {
+            float timeRemaining = totalSeconds - elapsedTime;
+            SendMessageUpwards("OnCalculateFinalScore", timeRemaining);
+        }
+    }
+
+    // Note: using int arg as uint didn't appear in Unity property inspector
+    public void OnNextTapped(int attempts)
+    {
+        OnNext((uint)attempts); // convert back to uint
+    }
+
+}
